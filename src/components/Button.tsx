1f571f879251b2125143c16a07b40220113fd262
amlsalisson@gmail.com <amlsalisson@gmail.com>
2021-06-26 19:25:57 -0300

    Aula 5 NLW

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,21 @@
 import { ButtonHTMLAttributes } from 'react'
 
-import'../styles/button.scss';
+import '../styles/button.scss';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    isOutLined?: boolean
+};
 
 
 
-export function Button(props: ButtonProps) {
+export function Button({ isOutLined = false, ...props }: ButtonProps) {
     return (
-        <button className="button" {...props} />
-
+        <button
+            className={`button ${isOutLined ? 'outLined' : ''}`}
+            {...props}
+        />
     )
 }
 
-<Button />
+
 
